f183b495cc0ee21f9296c7fedcd05cf83ac5eba8
shaku1z <shatheartboy@gmail.com>
2026-07-18 15:59:03 -0400

    fix(seo): update canonical URL to point to wiki.tearblade.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
-	site: process.env.SITE_URL || 'https://tear-wiki.pages.dev',
+	site: process.env.SITE_URL || 'https://wiki.tearblade.com',
 	prefetch: true,
 	integrations: [
 		starlight({
